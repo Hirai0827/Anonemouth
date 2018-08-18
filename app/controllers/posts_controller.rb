11033c8b28c7543debc6921b03c10a2470cf5d3f
hirai <ryu1999827@gmail.com>
2018-08-18 23:45:19 +0900

    final

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,32 @@
 class PostsController < ApplicationController
   require 'securerandom'
   def show
-    random = Random.new()
-    id = [-1,-1,-1]
-    i = 0
-    while i < 4
-      id[i] = random.rand(Post.first.id..Post.last.id)
-      if Post.find_by(id:id[i]) == nil
-        i -= 1
-      else
-        if i != 0
-          for j in 0..(i-1) do
-            if id[j] == id[i]
-              i -= 1
+    if Post.count > 4
+      random = Random.new()
+      id = [-1,-1,-1]
+      i = 0
+      while i < 4
+        id[i] = random.rand(Post.first.id..Post.last.id)
+        if Post.find_by(id:id[i]) == nil
+          i -= 1
+        else
+          if i != 0
+            for j in 0..(i-1) do
+              if id[j] == id[i]
+                i -= 1
+              end
             end
           end
         end
+        i += 1
       end
-      i += 1
+      @Posts = [Post.find_by(id:id[0]),Post.find_by(id:id[1]),Post.find_by(id:id[2]),Post.find_by(id:id[3])]
+
+    else
+      @Posts = Post.all
     end
-    @Posts = [Post.find_by(id:id[0]),Post.find_by(id:id[1]),Post.find_by(id:id[2]),Post.find_by(id:id[3])]
-    @Favs = Fav.all
-  end
+      @Favs = Fav.all
+    end
   def create
     @message = ""
   end
